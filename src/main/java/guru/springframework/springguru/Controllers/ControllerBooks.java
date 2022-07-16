971a9d620c4df54f40c1c6cbcb052d6a1ee12b12
Andrey <xramovic@yandex.ru>
2022-07-16 13:36:13 +0500

    Spring_Web.MVC.14

---
+++ src/main/java/guru/springframework/springguru/Controllers/ControllerBooks.java
@@ -0,0 +1,25 @@
+package guru.springframework.springguru.Controllers;
+
+
+import guru.springframework.springguru.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ControllerBooks {
+
+    private final BookRepository bookRepository;
+
+    public ControllerBooks(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBook(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+
+
+        return "books";
+    }
+}
